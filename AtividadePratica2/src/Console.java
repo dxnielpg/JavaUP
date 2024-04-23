f5732fee7f50a50f49c7c75ef4fceec6227fa0b0
Daniel Pereira Gonçalves <danielpgo11@gmail.com>
2024-04-22 21:44:41 -0300

    Leitor

--- AtividadePratica2/src/Console.java
+++ AtividadePratica2/src/Console.java
@@ -1,5 +1,28 @@
 public class Console {
     
+    import java.util.Scanner;
 
+    public class Console {
+    
+        private static Scanner leitor = new Scanner(System.in);
+    
+        public static String lerString() {
+            String valor = leitor.nextLine();
+            return valor;
+        }
+    
+        public static int lerInt() {
+            int valor = leitor.nextInt();
+            leitor.nextLine();
+            return valor;
+        }
+    
+        public static float lerFloat() {
+            float valor = leitor.nextFloat();
+            leitor.nextLine();
+            return valor;
+        }
+    
+    }
     
 }
